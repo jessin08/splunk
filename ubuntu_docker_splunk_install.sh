dd12d0f0a60eaf59197337a6d6812b0fc84b8da0
jessin08 <jessinrules@gmail.com>
2020-03-03 21:04:26 +0100

    Create ubuntu_docker_splunk_install.sh

---
+++ ubuntu_docker_splunk_install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#docker command: docker run -d -p 8000:8000 --name ubuntu ubuntu
+#docker command: docker exec -it ubuntu
+
+apt-get update
+echo "Installing wget and VIM"
+apt-get install wget vim
+echo "Downloading and extracting splunk"
+wget -O splunk-8.0.2-a7f645ddaf91-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.0.2&product=splunk&filename=splunk-8.0.2-a7f645ddaf91-Linux-x86_64.tgz&wget=true'
+mv splunk-8.0.2-a7f645ddaf91-Linux-x86_64.tgz /opt/splunk-8.0.2-a7f645ddaf91-Linux-x86_64.tgz
+tar -xvzf splunk-8.0.2-a7f645ddaf91-Linux-x86_64.tgz
+cd splunk-8.0.2-a7f645ddaf91-Linux-x86_64
+echo "removing splunk installation file"
+rm -rf ../splunk-8.0.2-a7f645ddaf91-Linux-x86_64.tgz
+
