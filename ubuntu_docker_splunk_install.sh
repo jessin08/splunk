1f94c95247a928c93cbe580150a1618507080a74
jessin08 <jessinrules@gmail.com>
2020-03-03 21:27:00 +0100

    Correct splunk directory location

--- ubuntu_docker_splunk_install.sh
+++ ubuntu_docker_splunk_install.sh
@@ -10,7 +10,7 @@ echo -e "\n---------------------------------\nDownloading and extracting splunk\
 wget -O splunk-8.0.2-a7f645ddaf91-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.0.2&product=splunk&filename=splunk-8.0.2-a7f645ddaf91-Linux-x86_64.tgz&wget=true'
 mv splunk-8.0.2-a7f645ddaf91-Linux-x86_64.tgz /opt/splunk-8.0.2-a7f645ddaf91-Linux-x86_64.tgz
 tar -xvzf splunk-8.0.2-a7f645ddaf91-Linux-x86_64.tgz
-cd /opt/splunk-8.0.2-a7f645ddaf91-Linux-x86_64
+cd /opt/splunk
 echo -e "\n---------------------------------\nremoving splunk installation file\n---------------------------------\n"
 rm -rf ../splunk-8.0.2-a7f645ddaf91-Linux-x86_64.tgz
 
